8a6d736a4d2f7ab06e5fdeac28eae7b7245c613f
JunjieC27 <jackychen27@icloud.com>
2016-12-04 14:58:53 -0500

    Completed upload_main layout

--- app/src/main/java/jackiechans/recipeek/Upload_main.java
+++ app/src/main/java/jackiechans/recipeek/Upload_main.java
@@ -11,37 +11,36 @@ import android.widget.*;
 import android.support.v7.app.AlertDialog;
 import android.content.*;
 import android.support.*;
+
+import java.util.LinkedList;
+
 //Junjie Chen, University of Ottawa ,2016-12-04
 
 public class Upload_main extends AppCompatActivity {
+    LinkedList<Integer> ingredientNameID= new LinkedList();
+    LinkedList<Integer> ingredientQuantityID= new LinkedList();
+    LinkedList<Integer> stepID= new LinkedList();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_main);
-        // following code will make dynamical edit text
-
-
     }
 
     public void createActivityDone(View view) {
         // this will check no empty edit text before uploaded
+        //get the results and store in a linkedList
+
         EditText titleEdit = (EditText) findViewById(R.id.titleEditText);
-        String step1 = ((EditText) findViewById(R.id.editText17)).getText().toString();
-        String step2 = ((EditText) findViewById(R.id.editText18)).getText().toString();
-        String step3 = ((EditText) findViewById(R.id.editText19)).getText().toString();
-        String step4 = ((EditText) findViewById(R.id.editText20)).getText().toString();
-        String step5 = ((EditText) findViewById(R.id.editText21)).getText().toString();
 
         String title = titleEdit.getText().toString();
         String done_error_message = "Your recipe must have title, ingredients, cooking time , and at least one step";
         String done_error_title = "Error!";
 
-        boolean hasAtLeastOneStep = (!step1.isEmpty())||(!step2.isEmpty())||(!step3.isEmpty())||(!step4.isEmpty())||(!step5.isEmpty());
 
-
-        if (title.isEmpty() || (!hasAtLeastOneStep)) {
+        if (title.isEmpty()) {//check content is not empty
             AlertDialog.Builder builder = new AlertDialog.Builder(Upload_main.this);
             builder.setMessage(done_error_message)
                     .setTitle(done_error_title)
@@ -54,6 +53,7 @@ public class Upload_main extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
     public void backToMain(View view){
         this.finish();
     } // OnClick method, go to main UI
@@ -67,8 +67,8 @@ public class Upload_main extends AppCompatActivity {
         builder.setTitle("Add Photo!");
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int item) {
-                if (items[item].equals("Take Photo")) {
+        public void onClick(DialogInterface dialog, int item) { //dummy dialog
+               if (items[item].equals("Take Photo")) {
         } else if (items[item].equals("Choose from Library")) {
         } else if (items[item].equals("Cancel")) {
             dialog.dismiss();
@@ -79,22 +79,31 @@ public class Upload_main extends AppCompatActivity {
     }
     public void cameraIntent(){
         // this will open the camera
-
-
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,0);
     }
+
     public void MoreIngredient(View view){ //OnClick to add more ingredients
         LinearLayout mLayout = (LinearLayout) findViewById(R.id.ingredientContainer);
-        EditText mEditText = (EditText) findViewById(R.id.ingredient_name_0);
-        int width = mEditText.getWidth();
-        mLayout.addView(createNewEditText(mEditText.getText().toString()));
-
-//asdasdasd
-
+        LinearLayout linearLayout = createLinear();
+        mLayout.addView(linearLayout);
+        EditText ingredientEditText = createNewEditText();
+        EditText quantityEditText = createNewEditText();
+        ingredientEditText.setHint("Add ingredient");
+        quantityEditText.setHint("Quantity");
+        linearLayout.addView(ingredientEditText);
+        linearLayout.addView(quantityEditText);
+        ingredientNameID.add(ingredientEditText.getId());
+        ingredientQuantityID.add(quantityEditText.getId());
     }
 
-    private EditText createNewEditText(String text) {
+    private LinearLayout createLinear(){
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        return linearLayout;
+    }
+
+    private EditText createNewEditText() {
     // this will create Edit Text dynamically
         ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         EditText newtext = new EditText(this);
@@ -103,8 +112,24 @@ public class Upload_main extends AppCompatActivity {
         newtext.setMinimumHeight(50);
         return newtext;
     }
+    public void moreStep(View view){ //OnClick , Add more steps
+        LinearLayout mLayout = (LinearLayout) findViewById(R.id.steps_container);
+        EditText stepEditText =createNewStepEditText();
+        mLayout.addView(stepEditText);
+        stepID.add(stepEditText.getId());
+    }
+    private EditText createNewStepEditText() { //this will creat step editText
+        // this will create Edit Text dynamically
+        ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        EditText newtext = new EditText(this);
+        newtext.setLayoutParams(lparams);
+        newtext.setWidth(700);
+        newtext.setMinimumHeight(50);
+        newtext.setHint("Add Step");
+        return newtext;
+    }
     /*
-    private void galleryIntent()
+    private void galleryIntent() // this will open the gallery
     {
         Intent intent = new Intent();
         intent.setType("image/*");
