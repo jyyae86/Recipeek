0a5fd2e4adeb7ceb6752a870014b16bbf7ba9eee
LinyangZhang <lzhan148@uottawa.ca>
2016-12-05 10:23:08 -0500

    Linyang part 1

--- app/src/main/java/jackiechans/recipeek/MyRecipe.java
+++ app/src/main/java/jackiechans/recipeek/MyRecipe.java
@@ -1,15 +1,49 @@
 package jackiechans.recipeek;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jyyae86 on 2016-11-30.
  */
 
-public class MyRecipe extends AppCompatActivity{
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_recipes);
+
+public class MyRecipe extends AppCompatActivity {
+    private ListView myList;
+
+    public void onCreate(Bundle saveInstanceState) {
+        setContentView(R.layout.your_layout);
+
+        myList = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                myRecipeList);
+
+        myList.setAdapter(arrayAdapter);
+    }
+
+    //created recipes are stored in an array
+    private Recipe[] recipe = new Recipe[1000];
+    private int numOfRecipes = 0;
+    ArrayList<String> myRecipeList=new ArrayList<String>();
+
+    public void addRecipe(Recipe newRecipe) {
+        recipe[numOfRecipes] = newRecipe;
+        myRecipeList.add(newRecipe.title);
+        numOfRecipes++;
+        return;
     }
 }
