a19a977d760d3e7ea732f196592d23dbb0bf4fe7
JunjieC27 <jackychen27@icloud.com>
2016-12-05 11:32:10 -0500

    Revert "Merge remote-tracking branch 'origin/master' into fuck"
    This reverts commit b9c875650b013798747cbf6157f3f4caa634373f, reversing
    changes made to 69b09dc4e61d8d7a072dfef3fa2d57ade530017d.

--- app/src/main/java/jackiechans/recipeek/FavouriteRecipe.java
+++ app/src/main/java/jackiechans/recipeek/FavouriteRecipe.java
@@ -1,47 +1,17 @@
 package jackiechans.recipeek;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by jyyae86 on 2016-11-30.
  */
 
-
-public class FavouriteRecipe extends AppCompatActivity {
-    private ListView favList;
-
-    public void onCreate(Bundle saveInstanceState) {
-        setContentView(R.layout.your_layout);
-        favList = (ListView) findViewById(R.id.listFavView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                favRecipes);
-
-        favList.setAdapter(arrayAdapter);
+public class FavouriteRecipe extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_recipes);
     }
 
-    //favourited recipes are stored in an array
-    private Recipe[] favRecipe = new Recipe[1000];
-    private int numOfFavRecipes = 0;
-    ArrayList<String> favRecipes=new ArrayList<String>();
 
-    public void addFavRecipe(Recipe newRecipe) {
-        favRecipe[numOfFavRecipes] = newRecipe;
-        favRecipes.add(newRecipe.title);
-        numOfFavRecipes++;
-        return;
-    }
 }
