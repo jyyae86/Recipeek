fe6587053404f98652a877a4f79ae3178d834a6e
Iain Jiang <jyyae86@gmail.com>
2016-12-07 20:09:40 -0500

    Added Search function

--- app/src/main/java/jackiechans/recipeek/SearchHome.java
+++ app/src/main/java/jackiechans/recipeek/SearchHome.java
@@ -1,8 +1,10 @@
 package jackiechans.recipeek;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -11,7 +13,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.LinkedList;
 
-import static jackiechans.recipeek.MainActivity.AllRecipe;
 import static jackiechans.recipeek.R.id.booleanSelect;
 import static jackiechans.recipeek.R.id.ingredientBox1;
 import static jackiechans.recipeek.R.id.ingredientBox2;
@@ -42,7 +43,7 @@ public class SearchHome extends AppCompatActivity {
         foodType = (Spinner)findViewById(recipeCategory);
     }
 
-    public void search(){
+    public void searchMyFuckingApp(View view){
         String[] searchParams = new String[5];
         searchParams[0] = firstIngred.getText().toString();
         searchParams[1] = secondIngred.getText().toString();
@@ -50,6 +51,9 @@ public class SearchHome extends AppCompatActivity {
         searchParams[3] = country.getSelectedItem().toString();
         searchParams[4] = foodType.getSelectedItem().toString();
         searchFirstParam(searchParams);
+        Intent intent = new Intent(this, ListOfSearch.class);
+        startActivity(intent);
+
     }
 
     public String getBoolean(){
@@ -72,17 +76,107 @@ public class SearchHome extends AppCompatActivity {
         return s;
     }
 
-    protected void searchFirstParam(String[] searchParams){
-        //Loop through all of the recipes and on each recipe, loop through the ingredients and add the recipe to the results linkedlist if
+    protected LinkedList<Recipe> searchIngred(String param, LinkedList<Recipe> recipes){
+        //Loop through all of the recipes and on each recipe, loop through the ingredients and add the recipe to the results linkedlist if the ingredient exists
+
         LinkedList<Recipe> result = new LinkedList<Recipe>();
-        for(int i = 0; i < AllRecipe.size(); i++){
-            Recipe temp = MainActivity.AllRecipe.get(i);
+        for(int i = 0; i < recipes.size(); i++){
+            Recipe temp = recipes.get(i);
+            Boolean added = false;
             for(int j = 0; j < temp.getIngredientList().size(); j++){
-                if(temp.getIngredientList().get(j).getName().equals(searchParams[0])){
+                if(temp.getIngredientList().get(j).getName().equals(param) && !added){
+                    added = true;
                     result.add(temp);
                 }
             }
         }
+        return result;
     }
+
+    protected void searchFirstParam(String[] searchParams){
+        LinkedList<Recipe> result = searchIngred(searchParams[0], MainActivity.AllRecipe);
+        searchSecondParam(searchParams, result);
+    }
+
+    protected void searchSecondParam(String[] searchParam, LinkedList<Recipe> list){
+        //if the bool op is and, it will search the previous linkedlist
+        //if the bool op is or, it will search from the allRecipes linkedlist and then merge them together
+        //if the bool is not, it will search the previous linkedlist for all that does not contain the ingredient
+        String ingred = searchParam[1];
+        LinkedList<Recipe> result = new LinkedList<Recipe>();
+        if(searchParam[1].equals("")){
+            result = list;
+        }else{
+            if(searchParam[2].equals("And")){
+                result = searchIngred(ingred, list);
+            }else if(searchParam[2].equals("Or")){
+                LinkedList<Recipe> tempOrResults = searchIngred(ingred, MainActivity.AllRecipe);
+                result = mergeRecipeLists(list, tempOrResults);
+            }else{
+                //loop through the objects to see if the ingredient exists, if it does, remove that element
+                for(int i = 0; i < list.size(); i++){
+                    Recipe temp = list.get(i);
+                    for(int j = 0; j < temp.getIngredientList().size(); j++){
+                        if(temp.getIngredientList().get(j).getName().equals(ingred)){
+                            list.remove(i);
+                        }
+                    }
+                }
+                result = list;
+            }
+        }
+        searchCountry(searchParam, result);
+    }
+
+    protected void searchCountry(String[] searchParam, LinkedList<Recipe> list){
+        LinkedList<Recipe> tempList = new LinkedList<Recipe>();
+        if(searchParam[3].equals("N/A")){
+            tempList = list;
+        }else{
+            //loop through all the recipes in list and find all of them that include the country
+            for(int i = 0; i < list.size(); i++){
+                Recipe tempRecipe = list.get(i);
+                Boolean added = false;
+                if(tempRecipe.getCountry().equals(searchParam[3]) && !added){
+                    tempList.add(tempRecipe);
+                    added = true;
+                }
+            }
+        }
+        searchFoodType(searchParam, tempList);
+    }
+
+    protected void searchFoodType(String[] searchParam, LinkedList<Recipe> list){
+        LinkedList<Recipe> tempList = new LinkedList<Recipe>();
+        if(searchParam[4].equals("N/A")){
+            tempList = list;
+        }else{
+            //loop through all the recipes in list and find all of them that include the country
+            Boolean added = false;
+            for(int i = 0; i < list.size(); i++) {
+                Recipe tempRecipe = list.get(i);
+                if (tempRecipe.getCategory().equals(searchParam[4]) && !added) {
+                    tempList.add(tempRecipe);
+                    added = true;
+                }
+            }
+        }
+        MainActivity.resultsRecipe = tempList;
+    }
+
+    protected LinkedList<Recipe> mergeRecipeLists(LinkedList<Recipe> l1, LinkedList<Recipe> l2){
+        //merges the two linked lists together l1 first then l2
+        LinkedList<Recipe> result = new LinkedList<Recipe>();
+        for(int i = 0; i < l1.size(); i++){
+            result.add(l1.get(i));
+        }
+
+        for(int i = 0; i < l2.size(); i++){
+            result.add(l2.get(i));
+        }
+        return result;
+    }
+
+
 
 }
