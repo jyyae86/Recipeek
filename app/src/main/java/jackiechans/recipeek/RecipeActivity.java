8420fbf94e750eae1bbe78e6467a759317ed6f67
Q97 <jqiu1997@gmail.com>
2016-12-07 20:59:04 -0500

    deleted unused imports

--- app/src/main/java/jackiechans/recipeek/RecipeActivity.java
+++ app/src/main/java/jackiechans/recipeek/RecipeActivity.java
@@ -1,14 +1,11 @@
 package jackiechans.recipeek;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.LinkedList;
