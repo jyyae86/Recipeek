fb2761da12f1dc6eaaea67a467a8dec9ff04e7c4
Q97 <jqiu1997@gmail.com>
2016-12-07 21:32:14 -0500

    oh sweet jesus please work (3)

--- app/src/main/java/jackiechans/recipeek/RecipeActivity.java
+++ app/src/main/java/jackiechans/recipeek/RecipeActivity.java
@@ -53,7 +53,7 @@ public class RecipeActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.textInstructions);
         tv.setText("");
         for (Integer i = 1 ; i < instructions.size()+1; i++){
-            tv.append(i.toString() + ". " +instructions.get(i) +"/n/n");
+            tv.append(i.toString() + ". " +instructions.get(i) +"\n\n");
         }
 
     }
@@ -63,7 +63,7 @@ public class RecipeActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.textIngredients);
         tv.setText("");
         for (int i = 0; i < ingredient.size(); i++){
-            tv.append("•" + ingredient.get(i).getQuantity() + " " + ingredient.get(i).getName() + "/n");
+            tv.append("•" + ingredient.get(i).getQuantity() + " " + ingredient.get(i).getName() + "\n");
         }
 
     }
